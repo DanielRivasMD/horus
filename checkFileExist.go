b45c9a43dfc6faa84d95ddf768e844db3e6c567b
Daniel Rivas <danielrivasmd@gmail.com>
2025-05-02 08:04:43 -0600

    add facultative logging check file exist

--- checkFileExist.go
+++ checkFileExist.go
@@ -1,4 +1,6 @@
 ////////////////////////////////////////////////////////////////////////////////////////////////////
+// package horus
+////////////////////////////////////////////////////////////////////////////////////////////////////
 
 package horus
 
@@ -10,26 +12,30 @@ import (
 )
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
-
 // CheckFileExist checks if a file exists. If it doesn't, it executes
 // the provided notFoundAction. Any error from the stat operation or the
 // notFoundAction will be wrapped in an Herror.
-func CheckFileExist(filePath string, notFoundAction NotFoundAction) error {
+// The verbose flag enables optional logging.
+func CheckFileExist(filePath string, notFoundAction NotFoundAction, verbose bool) error {
 	_, err := os.Stat(filePath)
 	if err != nil {
 		if os.IsNotExist(err) {
-			fmt.Printf("File '%s' does not exist. Executing custom action.\n", filePath)
+			if verbose {
+				fmt.Printf("File '%s' does not exist. Executing custom action.\n", filePath)
+			}
 			if notFoundAction != nil {
 				if actionErr := notFoundAction(filePath); actionErr != nil {
 					return NewHerror("check file", fmt.Sprintf("'%s' not found action failed", filePath), actionErr, map[string]any{"path": filePath})
 				}
-				return nil // Action succeeded, no error to report for the check itself
+				return nil // Action succeeded, no error to report for the check itself.
 			}
 			return NewHerror("check file", fmt.Sprintf("file '%s' not found", filePath), nil, map[string]any{"path": filePath, "action": "none"})
 		}
 		return NewHerror("check file", fmt.Sprintf("error checking file '%s'", filePath), err, map[string]any{"path": filePath})
 	}
-	fmt.Printf("File '%s' exists: '%s'\n", filePath, filePath)
+	if verbose {
+		fmt.Printf("File '%s' exists: '%s'\n", filePath, filePath)
+	}
 	return nil
 }
 
